e890681d4d6141e140dee53225c7e82370543c5d
Đạt Dev <datamity@gmail.com>
2019-07-19 18:17:28 +0700

    [FIX] sua vue-ra

---
+++ src/components/OrgChart.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <CsOrgChart
+              :tree="tree"
+              :nodeWidth="180"
+              :nodeHeight="100"
+              :nodeSpacing="12"
+              :animationDuration="350"
+              lineType="angle"
+    />
+  </div>
+</template>
+
+<script>
+  import CsOrgChart from "../OrgChart/src/index.js";
+  import fakeData from '../OrgChart/src/utils/fake-data';
+
+  export default {
+    name: 'OrgChart',
+
+    components: {
+      CsOrgChart
+    },
+
+    data(){
+      return {
+        tree: fakeData()
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { VuePlugin } from 'vuera'
+
+Vue.use(VuePlugin)
 
 Vue.config.productionTip = false
 
